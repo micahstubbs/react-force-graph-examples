3b27bfe478881ee2ce2fede8264d31ebb931e915
Micah Stubbs <micah.stubbs@gmail.com>
2018-07-02 00:45:44 -0700

    add linked-view

---
+++ 2d-3d-linked-view/00/README.md
@@ -0,0 +1,7 @@
+a linked view, with
+
+ForceGraph2D on the left | ForceGraph3D on the right
+
+drag a node in one pane to see the graph layout update in the other ✨
+
+based on this example from https://github.com/vasturiano/react-force-graph/blob/master/example/all-modes/index.html
---
+++ 2d-3d-linked-view/00/vis.jsx
@@ -0,0 +1,13 @@
+fetch('miserables.json')
+  .then(res => res.json())
+  .then(data => {
+    const comps = [ForceGraph.ForceGraph2D, ForceGraph.ForceGraph3D]
+    const compWidth = window.innerWidth / comps.length
+
+    ReactDOM.render(
+      <div style={{ display: 'flex' }}>
+        {comps.map(Comp => <Comp width={compWidth} graphData={data} />)}
+      </div>,
+      document.getElementById('graph')
+    )
+  })
