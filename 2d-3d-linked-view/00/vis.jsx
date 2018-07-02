ab2948fd81986a6e8a035f5bd4c628e079d73932
Micah Stubbs <micah.stubbs@gmail.com>
2018-07-02 00:49:12 -0700

    add explanatory comment

--- 2d-3d-linked-view/00/vis.jsx
+++ 2d-3d-linked-view/00/vis.jsx
@@ -1,12 +1,18 @@
 fetch('miserables.json')
   .then(res => res.json())
   .then(data => {
-    const comps = [ForceGraph.ForceGraph2D, ForceGraph.ForceGraph3D]
-    const compWidth = window.innerWidth / comps.length
+    const components = [ForceGraph.ForceGraph2D, ForceGraph.ForceGraph3D]
+    const componentWidth = window.innerWidth / components.length
 
+    // same data structure `data` is passed by reference to each component
+    // on interaction (like dragging a node),
+    // the component that the user interacts with mutates the shared `data`
+    // which causes both components to re-render
     ReactDOM.render(
       <div style={{ display: 'flex' }}>
-        {comps.map(Comp => <Comp width={compWidth} graphData={data} />)}
+        {components.map(Component => (
+          <Component width={componentWidth} graphData={data} />
+        ))}
       </div>,
       document.getElementById('graph')
     )
